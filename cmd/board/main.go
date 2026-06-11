992d1e56638caf927d00331cbd6843e6db49f996
Jordan Herge <lethalbacon16@gmail.com>
2026-06-11 17:33:29 +0000

    finish http response helpers

--- cmd/board/main.go
+++ cmd/board/main.go
@@ -5,7 +5,6 @@ import (
 	"diplomacy-api/internal/game"
 	"diplomacy-api/internal/http"
 	"diplomacy-api/internal/platform/mongo"
-	"encoding/json"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -14,29 +13,21 @@ import (
 func handler(ctx context.Context, event events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
 	db, err := mongo.NewMongoDB(ctx)
 	if err != nil {
-		http.InternalServerError()
+		return http.InternalServerError(&http.Error{
+			Message: err.Error(),
+		}), err
 	}
 
 	gameRepo := game.MakeRepository(db)
 
 	game, err := gameRepo.Get(ctx, "69564bb933c5739468982b67")
 	if err != nil {
-		return events.APIGatewayV2HTTPResponse{
-			StatusCode: 500,
-		}, err
+		return http.InternalServerError(&http.Error{
+			Message: err.Error(),
+		}), err
 	}
 
-	body, err := json.Marshal(game)
-	if err != nil {
-		return events.APIGatewayV2HTTPResponse{
-			StatusCode: 500,
-		}, err
-	}
-
-	return events.APIGatewayV2HTTPResponse{
-		StatusCode: 200,
-		Body:       string(body),
-	}, nil
+	return http.Ok(game), nil
 }
 
 func main() {
